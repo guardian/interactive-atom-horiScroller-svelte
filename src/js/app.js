331ff8f8e4aa54a05450b98c3edba5ca11e45bf9
cfen <chris.fenn@guardian.co.uk>
2017-08-10 18:01:09 +0100

    Added hide intro for mobile plus easing on scroll

--- src/js/app.js
+++ src/js/app.js
@@ -4,9 +4,10 @@ let H;
 let screenFormat;
 let wrapEl;
 let headEl;
+let introPanelEl;
 let widthsArr = [];
 let scrollerEl;
-let introEl;
+let introCaptionEl;
 let introPlayed = false;
 
 console.log(lazysizes)
@@ -17,8 +18,8 @@ function init() {
     headEl = document.getElementById("bannerandheader");
     wrapEl = document.querySelector(".gv-wrap-all");
     scrollerEl = document.getElementById("slideScroller")
-    introEl = document.getElementById("introCappy");
-
+    introCaptionEl = document.getElementById("introCappy");
+    introPanelEl = document.querySelector(".gv-intro-panel");
     checkScreenSize();
 }
 
@@ -27,16 +28,12 @@ function checkScreenSize() {
     screenFormat == "desktop" ? initDesktop() : initMobile();
 }
 
-
 function initMobile() {
-    // wrapAll.setAttribute("style","height:"+H+"px");
-    // console.log()
-    // addListenersDesktop();
-    let mobH = document.documentElement.offsetHeight - 300;
-    document.querySelector(".gv-fade-out").classList.add("remove")
-        // document.querySelector("#slideWrapper").style.height = mobH +"px"
+    introPanelEl.classList.add("fade-half")
     console.log("mobile");
-    introEl.style.bottom = "0px";
+    introCaptionEl.style.bottom = "0px";
+
+        addListenersMobile();
 }
 
 
@@ -60,7 +57,7 @@ function initDesktop() {
 
                     wrapEl.setAttribute("style", "height:" + (H - document.documentElement.offsetWidth) + "px");
 
-                    document.querySelector(".gv-fade-out").classList.add("remove")
+                    introPanelEl.classList.add("fade-half")
 
                 }
 
@@ -68,19 +65,10 @@ function initDesktop() {
 
         });
 
-
     });
 
     addListenersDesktop();
 
-}
-
-function getScroll() {
-    var target = $("#target");
-    $("#source").scroll(function() {
-        target.prop("scrollTop", this.scrollTop)
-            .prop("scrollLeft", this.scrollLeft);
-    });
 }
 
 var noTweak = true;
@@ -91,68 +79,72 @@ function addListenersDesktop() {
 
         document.getElementById('slideWrapper').classList.add('fixed');
 
-
-        //scrollerEl.scrollLeft = document.documentElement.scrollTop;
-
         if (noTweak) {
             let newH = wrapEl.getBoundingClientRect().height + (scrollerEl.getBoundingClientRect().height); //- headEl.offsetHeight
             wrapEl.setAttribute("style", "height:" + newH + "px");
             noTweak = false;
         }
 
-        //console.log(headEl.offsetHeight +"> = < ???" + wrapEl.getBoundingClientRect().top)
-
         if (notShownY(headEl)) {
             scrollerEl.style.transform = "translateX(" + (wrapEl.getBoundingClientRect().top) + "px)";
+            introPanelEl.classList.add("remove");
         } else {
             scrollerEl.style.transform = "translateX(0)";
+            introPanelEl.classList.remove("remove");
+            introPanelEl.classList.add("fade-half");
         }
 
     })
 
 }
 
+function addListenersMobile(){
+	document.addEventListener("scroll", function() {
+		introPanelEl.classList.add("remove");
 
-function scrollSlider(s) {
+	}
 
-    //console.log(s[1])
-    let newX = (0 - s[1]);
-
-    // if(newX > (0 - document.documentElement.offsetWidth)){ - document.documentElement.offsetWidth
-    scrollerEl.style.transform = "translateX(" + (newX - document.documentElement.offsetWidth) + "px)";
-
-    //}
-
-    var a = wrapEl.scrollTop;
-    var b = wrapEl.offsetHeight - wrapEl.scrollTop;
-    // var c = a/b; //% of scroll --- https://stackoverflow.com/questions/2481350/how-to-get-scrollbar-position-with-javascript
-    //console.log(a,b);
-
-    if ((headEl.getBoundingClientRect().top * -1) > 2000) {
-        introPlayed = false;
-    }
 }
 
-function scrollIntro(s) {
-    let newY = s[1];
-    let newPos = introEl.getBoundingClientRect().bottom;
-    newPos += newY;
-    if ((headEl.getBoundingClientRect().top * -1) > 2000) {
-        introPlayed = true;
-    } else {
-        introEl.style.transform = "translateY(300px)";
-
-        console.log(newPos)
-    }
-}
 
 function notShownY(el) {
     return (el.offsetHeight * -1) > el.getBoundingClientRect().top;
 }
 
-
 init();
 
+
+
+
+
+// function scrollSlider(s) {
+//     let newX = (0 - s[1]);
+//     scrollerEl.style.transform = "translateX(" + (newX - document.documentElement.offsetWidth) + "px)";
+
+//     //}
+
+//     var a = wrapEl.scrollTop;
+//     var b = wrapEl.offsetHeight - wrapEl.scrollTop;
+//     // var c = a/b; //% of scroll --- https://stackoverflow.com/questions/2481350/how-to-get-scrollbar-position-with-javascript
+//     //console.log(a,b);
+
+//     if ((headEl.getBoundingClientRect().top * -1) > 2000) {
+//         introPlayed = false;
+//     }
+// }
+
+// function scrollIntro(s) {
+//     let newY = s[1];
+//     let newPos = introCaptionEl.getBoundingClientRect().bottom;
+//     newPos += newY;
+//     if ((headEl.getBoundingClientRect().top * -1) > 2000) {
+//         introPlayed = true;
+//     } else {
+//         introCaptionEl.style.transform = "translateY(300px)";
+
+//         console.log(newPos)
+//     }
+// }
 
 ///////////////timer example
 
